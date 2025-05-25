7ef459caf6bbb9a99e434b18ab014f7896db4a80
Yehor Kosiak <yegorkosyak32@gmail.com>
2025-05-25 15:28:50 +0200

    Update Next.js configuration and package dependencies
    - Set output to "export", enabled trailing slashes, and defined basePath and assetPrefix in next.config.mjs.
    - Added a deploy script to package.json for gh-pages.
    - Updated React and date-fns versions in package.json.
    - Introduced package-lock.json for dependency management.
    - Removed pnpm-lock.yaml as part of the transition to npm.

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+  trailingSlash: true,
+  basePath: "/portfolio",
+  assetPrefix: "/portfolio/",
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -9,6 +13,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
